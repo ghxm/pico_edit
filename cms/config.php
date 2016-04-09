316e060b752d5da26b97b230e4bfc337e6414834
mat <mat@blocknot.es>
2016-04-09 18:58:43 +0200

    Updated

--- cms/config.php
+++ cms/config.php
@@ -6,6 +6,6 @@ global $backend_password;
  * This should be a sha1 hash of your password.
  * Use a tool like http://www.sha1-online.com to generate.
  */
-$backend_password = '21cf0cddb652c54cbcaaf1b0bbb2f5a7fe11f39d';
+$backend_password = '';
 
 ?>
